2d996f5ab6e0f97a9dbe6a0cdd9723b1260073d7
adityaa-12 <adityaa.sde@gmail.com>
2025-05-27 00:06:46 +0530

    updated the server and app for backend

---
+++ server/app.js
@@ -0,0 +1,46 @@
+// Express App
+
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import db from "./config/db.js";
+import bookRoutes from "./routes/bookRoutes.js"
+import reviewRoutes from "./routes/reviewRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import cookieParser from "cookie-parser";
+import { authUser } from "./middleware/auth.js";
+
+export default async function startApp() {
+  const app = express();
+
+  // Middlewares
+
+  app.use(express.json());
+  app.use(cors({
+    origin: "*",
+    credentials: true,
+  }));
+  app.use(morgan("short"));
+  app.use(cookieParser());
+
+  // Main routes
+
+  app.get("/", (req, res) => {
+    res.send("Server is Running!");
+  });
+
+  app.use("/books", bookRoutes);
+  app.use("/review", reviewRoutes);
+  app.use("/user", userRoutes);
+
+  // To check the db is active
+
+  try {
+    await db.query("SELECT 1");
+    console.log("Database connection is active!");
+  } catch (error) {
+    console.log("Database connection is failed", error.message);
+  }
+
+  return app;
+}
