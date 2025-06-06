b2eec4bb996b0489121aafc187a59f583ddac237
adityaa-12 <adityaa.sde@gmail.com>
2025-06-07 00:39:58 +0530

    updated routes for app

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -4,6 +4,7 @@ import RateBookHome from '../pages/RateBookHome';
 import SearchResults from '../pages/SearchResults';
 import Books from '../pages/Books';
 import AboutUs from '../pages/AboutUs';
+import UserDashboard from '../pages/UserDashboard';
 
 const AppRouter: React.FC = () => {
     return (
@@ -13,6 +14,9 @@ const AppRouter: React.FC = () => {
                 <Route path='/results' element={<SearchResults />} />
                 <Route path='/books' element={<Books />} />
                 <Route path='/about' element={<AboutUs />} />
+                
+                {/* Protected Client Links */}
+                <Route path='/user-dashboard' element={<UserDashboard />} />
             </Routes>
         </>
     )
