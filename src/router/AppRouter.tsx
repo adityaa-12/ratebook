20c297306b73079be164a91bff83a672200caafb
adityaa-12 <adityaa.sde@gmail.com>
2025-06-08 18:29:31 +0530

    added routes for client

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -5,18 +5,22 @@ import SearchResults from '../pages/SearchResults';
 import Books from '../pages/Books';
 import AboutUs from '../pages/AboutUs';
 import UserDashboard from '../pages/UserDashboard';
+import NotFound from '../pages/NotFound';
 
 const AppRouter: React.FC = () => {
     return (
         <>
             <Routes>
+                {/* Not Found Route */}
+                <Route path='*' element={<NotFound />} />
+                {/* Global Routes */}
                 <Route path='/' element={<RateBookHome />} />
                 <Route path='/results' element={<SearchResults />} />
                 <Route path='/books' element={<Books />} />
                 <Route path='/about' element={<AboutUs />} />
-                
+
                 {/* Protected Client Links */}
-                <Route path='/user-dashboard' element={<UserDashboard />} />
+                <Route path='/v2/dashboard' element={<UserDashboard />} />
             </Routes>
         </>
     )
