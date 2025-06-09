7ce9e2197837a104b88c0bcfa09268dfa27a6b02
adityaa-12 <adityaa.sde@gmail.com>
2025-06-09 17:37:01 +0530

    updated app layout

--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import { Outlet } from 'react-router-dom';
 
+
 const AppLayout: React.FC = () => {
     return (
         <div className='w-[85vw] mx-auto max-sm:w-[95vw]'>
