bcedfb3f96787454d8aa92f0ca95ed05c41571e2
adityaa-12 <adityaa.sde@gmail.com>
2025-06-09 17:34:48 +0530

    added loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Loading: React.FC = () => {
     return (
         <div>
-
-        </div>
+            <div id="loader" className='border-blue-500 border-t-transparent rounded-full w-10 h-10 border-4 animate-spin mx-auto my-36'></div>
+        </div>  
     )
 }
 
