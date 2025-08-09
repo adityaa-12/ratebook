ab1b866938ef11e9479cbb4b0f450fc1edae4dcc
adityaa-12 <adityaa.sde@gmail.com>
2025-08-09 21:05:23 +0530

    added more apis for book route

--- server/src/controllers/bookControl.js
+++ server/src/controllers/bookControl.js
@@ -1,5 +1,7 @@
 import books from "../models/bookModel.js";
 
+// Adding a new Book
+
 export const bookCreate = async (req, res) => {
   try {
     if (!req.body) {
@@ -9,11 +11,94 @@ export const bookCreate = async (req, res) => {
       return;
     }
     await books.create(req.body);
-    res.status(201).json({
+    return res.status(201).json({
       message: "Book Added Successfully",
     });
   } catch (error) {
-    res.status(400).json({
+    return res.status(400).json({
+      message: error.message,
+    });
+  }
+};
+
+// Getting all Books
+
+export const fetchBooks = async (req, res) => {
+  try {
+    const getBooks = await books.find();
+    return res.status(200).json({
+      message: "Success",
+      data: getBooks,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: error.message,
+    });
+  }
+};
+
+// Searching Books by Query
+
+export const searchBook = async (req, res) => {
+  try {
+    const query = String(req.query.q);
+
+    if (!query) {
+      return res.status(400).json({
+        message: "Search query required!",
+      });
+    }
+
+    const findBook = await books.find({
+      $or: [
+        { title: { $regex: query, $options: "i" } },
+        {
+          author: { $regex: query, $options: "i" },
+        },
+      ],
+    });
+
+    if (findBook == 0) {
+      return res.status(404).json({
+        message: "Not Found!",
+      });
+    } else {
+      return res.status(200).json({
+        message: "Success",
+        data: findBook,
+      });
+    }
+  } catch (error) {
+    return res.status(400).json({
+      message: error.message,
+    });
+  }
+};
+
+// Update a Book by ID
+
+export const updateBook = async (req, res) => {
+  try {
+    const bookId = req.params.id;
+    const updatedBook = req.body;
+
+    const updateBook = await books.findOneAndUpdate(
+      { id: bookId },
+      updatedBook,
+      { new: true }
+    );
+
+    if (!updateBook) {
+      return res.status(404).json({
+        message: "Not Found",
+      });
+    }
+    
+    return res.status(200).json({
+      message: "Book Updated Successfully!",
+    });
+  } catch (error) {
+    return res.status(400).json({
       message: error.message,
     });
   }
--- server/src/routes/bookRoutes.js
+++ server/src/routes/bookRoutes.js
@@ -1,8 +1,15 @@
 import express from "express";
-import { bookCreate } from "../controllers/bookControl.js";
+import { bookCreate, fetchBooks, searchBook, updateBook } from "../controllers/bookControl.js";
 
 const router = express.Router();
 
 router.post("/", bookCreate);
+
+router.get("/", fetchBooks);
+
+router.get("/search", searchBook);
+
+router.put("/update/:id", updateBook);
+
 
 export default router;
