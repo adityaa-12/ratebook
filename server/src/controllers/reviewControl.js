f33a4e064388b5282f4d0d245a4b2c4a07347e66
adityaa-12 <adityaa.sde@gmail.com>
2025-08-14 22:03:51 +0530

    added review model

---
+++ server/src/controllers/reviewControl.js
@@ -0,0 +1,7 @@
+export const createRev = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
---
+++ server/src/models/reviewModel.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const createReview = new mongoose.Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    authorid: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    userid: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export default mongoose.model("review", createReview);
